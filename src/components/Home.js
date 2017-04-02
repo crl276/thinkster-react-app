8fc504dcc5b2c65dbe9b5ea85842813312f53faf
Callan Lamb <callan.lamb@gmail.com>
2017-04-02 19:59:16 -0400

    Add component code

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,3 +6,30 @@ import { connect } from 'react-redux';
 const mapStateToProps = state => ({
 	appName: state.appName
 });
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div className="home-page">
+
+				<Banner appName={this.props.appName} />
+
+				<div className="container page">
+					<div className="row">
+						<MainView />
+
+						<div className="col-md-3">
+							<div className="sidebar">
+
+							<p>Popular Tags</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		);
+	}
+}
+
+export default connect(mapStateToProps, () => ({}))(Home);
