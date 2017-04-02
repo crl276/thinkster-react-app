6b8060c28ce86237fa52362b874a9d4474c12e4b
Callan Lamb <callan.lamb@gmail.com>
2017-04-02 13:07:39 -0400

    Add app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import App from './App';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { createStore } from 'redux';
+
+//set default state equal 
+const defaultState = { appName: 'CONDUIT' };
+const reducer = function(state = defaultState, action) {
+	switch (action.type) {
+		case 'TOGGLE':
+			return { ...state, checked: !state.checked };
+	}
+	return state;
+};
+const store = createStore(reducer);
+
+ReactDOM.render((
+  <Provider store={ store }>
+    <App />
+  </Provider>
+), document.getElementById('main'));
