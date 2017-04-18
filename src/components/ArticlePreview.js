1643324c6fc129253b50ed3445c27b9ae324d142
Callan Lamb <callan.lamb@gmail.com>
2017-04-17 22:03:32 -0400

    Add div header at the top

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -2,4 +2,13 @@ import React from 'react';
 
 const ArticlePreview = props => {
 	const article = props.article;
+
+	return (
+		<div className="article-preview">
+			<div className="article-meta">
+				<a>
+					<img src={article.author.image} />
+				</a>
+
+	)
 }
